f39bcb32fc976f413f29ff678f1b3e475bb6328e
KarW010 <kartikostudy@gmail.com>
2024-11-15 09:49:34 +0700

    Array1

--- ArrayNumbers09.java
+++ ArrayNumbers09.java
@@ -5,9 +5,8 @@ public class ArrayNumbers09 {
         num[1] = 12;
         num[2] = 7;
         num[3] = 20;
-        System.out.println(num[0]);
-        System.out.println(num[1]);
-        System.out.println(num[2]);
-        System.out.println(num[3]);
+        for (int i = 0; i < 4; i++) {
+            System.out.println(num[i]);
+        }
     }
-}
+} 
